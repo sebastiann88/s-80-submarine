42f49fb4b830669d5353065d275247cef3ad34c7
sebastiann88 <sebastian.nadeau45@gmail.com>
2025-04-24 22:56:58 -0400

    feat(all): create text fading effect

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
-import { gsap } from "./gsap-core.js";
-import { ScrollTrigger } from "./ScrollTrigger.min.js";
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".fade-text", {
+gsap.to("#view1", {
   opacity: 0,
   scrollTrigger: {
-    trigger: ".fade-text",
-    start: "top center", // when top of the text hits center of viewport
-    end: "bottom top",   // when bottom of the text hits top of viewport
+    trigger: "#view1",
+    start: "20% 20%", // when top of the text hits center of viewport
+    end: "bottom 70%",   // when bottom of the text hits top of viewport
     scrub: true,         // smooth scrub based on scroll position
+    // markers: true,       // show markers for debugging
+  }
+});
+
+gsap.to("#fadeLeft", {
+  xPercent: -100,
+  yPercent: -50,
+  scrollTrigger: {
+    trigger: "#view1",
+    start: "20% 20%", // when top of the text hits center of viewport
+    end: "bottom 70%",   // when bottom of the text hits top of viewport
+    scrub: true,         // smooth scrub based on scroll position
+    x: -400,
+    y: 0,
+    // markers: true,       // show markers for debugging
+  }
+});
+
+gsap.to("#fadeRight", {
+  xPercent: 100,
+  yPercent: 50,
+  scrollTrigger: {
+    trigger: "#view1",
+    start: "20% 20%", // when top of the text hits center of viewport
+    end: "bottom 70%",   // when bottom of the text hits top of viewport
+    scrub: true,         // smooth scrub based on scroll position
+    x: 400,
+    y: 0,
+    // markers: true,       // show markers for debugging
   }
 });
